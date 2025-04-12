684a7a59196dd504076c39787d98df0a18c9750e
Lea Lonchar <lea.lonchar@gmail.com>
2025-04-12 19:05:30 +0200

    Implemented count tasks per category

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -95,7 +95,7 @@ class TaskManager {
         return new ArrayList<>();
     }
 
-    // 7. Count tasks per category
+    7. Count tasks per category
     public Map<String, Integer> countTasksPerCategory() {
         // TODO: Implement counting logic
         return new HashMap<>();
